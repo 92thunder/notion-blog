e2fd857e86a91401ff82cd8fd0ffba75088ec108
ryota.kunisada <r.kunisada661@gmail.com>
2020-02-02 03:58:12 +0900

    fix og:title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,13 @@ import Head from 'next/head'
 const ogImageUrl = 'https://92thunder.dev/ogp.png'
 
 export default ({ titlePre = '' }) => {
+  const title = `${titlePre} | 92thunder blog`
   return (
     <header>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{title}</title>
         <meta name="description" content="92thunder blog" />
-        <meta name="og:title" content="92thunder blog" />
+        <meta name="og:title" content={title} />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@92thunder" />
         <meta name="twitter:card" content="summary_large_image" />
