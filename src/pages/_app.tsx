960b2879078a3ddcf411b7fae2e77dd8191a2a03
ryota.kunisada <r.kunisada661@gmail.com>
2020-02-02 23:59:17 +0900

    add next-ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/global.css'
-import ExtLink from '../components/ext-link'
+import App from 'next/app'
+import Router from 'next/router'
+import withGA from 'next-ga'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-  </>
-)
+class MyApp extends App {
+  // Only uncomment this method if you have blocking data requirements for
+  // every single page in your application. This disables the ability to
+  // perform automatic static optimization, causing every page in your app to
+  // be server-side rendered.
+  //
+  // static async getInitialProps(appContext) {
+  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  //   const appProps = await App.getInitialProps(appContext);
+  //
+  //   return { ...appProps }
+  // }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
+}
+export default withGA('UA-157573127-2', Router)(MyApp)
